8f6dc1397f388ba741dda829cb21b979dc06ce17
Raphaël Oliazadeh <Oliazadeh.raphael@gmail.com>
2024-01-26 14:27:28 +0100

    RED When_GetScore_Returns_Exception

---
+++ Bowling.Core/GameBowling.cs
@@ -0,0 +1,22 @@
+﻿namespace Bowling.Core
+{
+    public class GameBowling
+    {
+        // Stockage des lancer
+        private int[] _lancer = new int[21];
+        // Stockage du score
+        private int _score = 2;
+        // Stockage du nombre de tours viariable en fonction de strike/spare
+        private int _tours = 10;
+        // Stockage du nombre de quille
+        private int _quilles = 10;
+
+        public void GetScore()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+
+}
---
+++ BowlingTesting/Game.cs
@@ -0,0 +1,33 @@
+﻿using Bowling.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingTesting
+{
+    // Une qui vérifie si c'est un strike
+    // Une qui vérifie si c'est un spare
+    // Une qui vérifie si c'est un si elle ne sont pas toutes tombées
+    // Une qui vérifie si c'est un si il en a touché 1 ou + mais < que toutes
+    // Une qui vérifie si c'est un 
+
+    [TestFixture]
+
+    public class Game
+    {
+        [SetUp]
+        public GameBowling SetUp()
+        {
+            GameBowling game = new();
+            return game;
+        }
+
+        [Test]
+        public void When_GetScore_Returns_Exception()
+        {
+
+        }
+    }
+}
